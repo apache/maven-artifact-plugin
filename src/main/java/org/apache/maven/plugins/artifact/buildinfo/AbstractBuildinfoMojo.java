9372b9fe9e71b275c66f683fb7c50e03db93065f
Hervé Boutemy <hboutemy@apache.org>
2024-10-15 01:35:33 +0200

    [MARTIFACT-73] add describe-build-output experimental goal

--- src/main/java/org/apache/maven/plugins/artifact/buildinfo/AbstractBuildinfoMojo.java
+++ src/main/java/org/apache/maven/plugins/artifact/buildinfo/AbstractBuildinfoMojo.java
@@ -285,6 +285,14 @@ public abstract class AbstractBuildinfoMojo extends AbstractMojo {
         }
     }
 
+    protected BuildInfoWriter newBuildInfoWriter(PrintWriter p, boolean mono) {
+        BuildInfoWriter bi = new BuildInfoWriter(getLog(), p, mono, rtInformation);
+        bi.setIgnoreJavadoc(ignoreJavadoc);
+        bi.setIgnore(ignore);
+        bi.setToolchain(getToolchain());
+
+        return bi;
+    }
     /**
      * Generate buildinfo file.
      *
@@ -300,11 +308,7 @@ public abstract class AbstractBuildinfoMojo extends AbstractMojo {
 
         try (PrintWriter p = new PrintWriter(new BufferedWriter(
                 new OutputStreamWriter(Files.newOutputStream(buildinfoFile.toPath()), StandardCharsets.UTF_8)))) {
-            BuildInfoWriter bi = new BuildInfoWriter(getLog(), p, mono, rtInformation);
-            bi.setIgnoreJavadoc(ignoreJavadoc);
-            bi.setIgnore(ignore);
-            bi.setToolchain(getToolchain());
-
+            BuildInfoWriter bi = newBuildInfoWriter(p, mono);
             bi.printHeader(root, mono ? null : project, reproducible);
 
             // artifact(s) fingerprints
@@ -348,7 +352,7 @@ public abstract class AbstractBuildinfoMojo extends AbstractMojo {
         return null;
     }
 
-    private boolean isSkip(MavenProject project) {
+    protected boolean isSkip(MavenProject project) {
         // manual/configured module skip
         boolean skipModule = false;
         if (skipModules != null && !skipModules.isEmpty()) {
